cc89edd617a8a7f20d908aa3e5f9384d6231be9b
Caio Rodrigues <caio_rodrigues19@hotmail.com>
2018-10-22 23:25:51 -0300

    Todas Mudanças

--- sochacaras/trash(old files)/public/js/app_firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	apiKey: "AIzaSyBuET44EmovU_7py_DNvMj_rOmv_mKjXpg",
-	authDomain: "so-chacaras.firebaseapp.com",
-	databaseURL: "https://so-chacaras.firebaseio.com",
-	projectId: "so-chacaras",
-	storageBucket: "so-chacaras.appspot.com",
-	messagingSenderId: "841305546768"
-};
-firebase.initializeApp(config);
